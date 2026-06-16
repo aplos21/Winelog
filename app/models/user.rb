7d83abed9f44784856fa375ef37375b45256890d
mateus medeiros <mms1@icomp.ufam.edu.br>
2026-06-16 15:27:56 -0400

    Remove inactive_message customizado para evitar quebra de i18n

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
   has_many :wines, dependent: :destroy
   has_many :comments, dependent: :destroy
 
-  # Executa antes de salvar o usuário no banco pela primeira vez
+  # Pula a confirmação de e-mail e confirma no banco na hora do cadastro
   before_create :auto_confirm_user
 
   # Trava de login: Só entra se for aprovado por você
@@ -14,13 +14,8 @@ class User < ApplicationRecord
     super && approved?
   end
 
-  def inactive_message
-    approved? ? super : :not_approved
-  end
-
   private
 
-  # Pula a necessidade de enviar o e-mail do Devise, confirmando o usuário na hora
   def auto_confirm_user
     self.skip_confirmation!
   end
